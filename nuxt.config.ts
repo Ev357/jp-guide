05703fe636973490c616490c8619ba74563564b9
Ev357 <evest.dev@gmail.com>
2024-07-04 14:17:12 +0200

    build: init

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  compatibilityDate: "2024-07-04",
+  modules: [
+    "@nuxtjs/tailwindcss",
+    [
+      "shadcn-nuxt",
+      {
+        componentDir: "./app/components/ui",
+      },
+    ],
+  ],
+  future: {
+    compatibilityVersion: 4,
+  },
+  experimental: {
+    componentIslands: {
+      selectiveClient: "deep",
+    },
+  },
+});
