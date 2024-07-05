1d12892d93caaedd552596fff2510d9f47e9c9e6
Ev357 <evest.dev@gmail.com>
2024-07-05 17:49:31 +0200

    feat: hero card link

--- app/components/content/HeroCard.vue
+++ app/components/content/HeroCard.vue
@@ -1,12 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  path?: string;
+}>();
+</script>
+
 <template>
-  <Card class="not-prose">
-    <CardHeader>
-      <CardTitle>
-        <slot />
-      </CardTitle>
-      <CardDescription>
-        <slot name="description" />
-      </CardDescription>
-    </CardHeader>
-  </Card>
+  <NuxtLinkLocale :to="path">
+    <Card class="not-prose h-full">
+      <CardHeader>
+        <CardTitle>
+          <slot />
+        </CardTitle>
+        <CardDescription>
+          <slot name="description" />
+        </CardDescription>
+      </CardHeader>
+    </Card>
+  </NuxtLinkLocale>
 </template>
