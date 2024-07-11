fadf3f21329522eb86bfa2539c784a4fcad25e6f
Ev357 <evest.dev@gmail.com>
2024-07-11 16:30:18 +0200

    fix: paging

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -11,8 +11,6 @@ const path = computed(() => getPath(slug));
 
 const getFullPath = () => useRoute().fullPath;
 
-const basePath = computed(() => getBasePath(slug));
-
 const { data, error } = await useAsyncData<
   {
     page: ParsedContent;
@@ -29,7 +27,7 @@ const { data, error } = await useAsyncData<
     queryContent("/")
       .where({ _locale: locale.value, _path: path.value })
       .findOne(),
-    queryContent(basePath.value)
+    queryContent()
       .only(["_path", "title", "description"])
       .where({ _locale: locale.value })
       .findSurround(path.value),
